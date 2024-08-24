822073a3ad806d7d8e1bbdcd2735a34a29ed0d12
Holanddez <elfosupremo@hotmail.com>
2024-08-24 20:09:52 -0300

    feat: 404 page style

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,7 @@
 export function NotFound () {
   return(
     <div className="h-screen flex justify-center items-center sm:text-4xl text-2xl divide-x">
-      <h2 className="p-4 font-bold">404</h2>
+      <h2 className="p-4 font-bold text-[#861efd]">404</h2>
       <p className="p-4 font-bold">Página não encontrada</p>
     </div>
   );
