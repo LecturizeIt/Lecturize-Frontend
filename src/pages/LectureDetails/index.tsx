b2f83479a47060323b38418b7b703e3b2a7affe8
Matheus7p <matheus.costa.7p@gmail.com>
2024-10-26 07:22:21 -0300

    feat: open modal for view metrics

--- src/pages/LectureDetails/index.tsx
+++ src/pages/LectureDetails/index.tsx
@@ -1,6 +1,13 @@
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { deleteLecture, fetchLectureById, fetchLectureParticipants, participateInLecture, unParticipateInLecture, viewLecture } from "../../api/lecture";
+import {
+  deleteLecture,
+  fetchLectureById,
+  fetchLectureParticipants,
+  participateInLecture,
+  unParticipateInLecture,
+  viewLecture,
+} from "../../api/lecture";
 import Navbar from "../../components/Navbar/Navbar.component";
 import Footer from "../../components/Footer/Footer.component";
 import { ErrorNotification } from "../../ui/ErrorNotification/ErrorNotification.ui";
@@ -14,7 +21,11 @@ import { SuccessNotification } from "../../ui/SucessNotification/SucessNotificat
 import { ILectureDetail } from "../../domain/models/lecture.model";
 import LoadingSpinner from "../../ui/Loading/Loading.ui";
 import MectricsSection from "../../components/MetricsSection/MetricsSection.component";
-import { PencilSquareIcon, TrashIcon } from "@heroicons/react/16/solid";
+import {
+  ChartBarIcon,
+  PencilSquareIcon,
+  TrashIcon,
+} from "@heroicons/react/16/solid";
 import ConfirmationDialog from "../../ui/ConfirmationDialog/ConfirmationDialog.ui";
 
 function LectureDetails () {
@@ -22,21 +33,37 @@ function LectureDetails () {
   const navigate = useNavigate();
   const { user } = useAuth();
   const queryClient = useQueryClient();
-  const [isEditModalOpen, setEditModalOpen] = useState(false); 
-  const [successMessage, setSuccessMessage] = useState<string | undefined>(undefined);
-  const [errorMessage, setErrorMessage] = useState<string | undefined>(undefined);
+  const [isEditModalOpen, setEditModalOpen] = useState(false);
+  const [isMetricModalOpen, setMetricModalOpen] = useState(false);
+  const [successMessage, setSuccessMessage] = useState<string | undefined>(
+    undefined
+  );
+  const [errorMessage, setErrorMessage] = useState<string | undefined>(
+    undefined
+  );
   const [isShowDialog, setShowDialog] = useState(false);
 
-
   const handleOpenEditModal = () => {
-    setEditModalOpen(true); 
+    setEditModalOpen(true);
   };
 
   const handleCloseEditModal = () => {
-    setEditModalOpen(false); 
+    setEditModalOpen(false);
   };
 
-  const { data: lecture, isLoading, isError } = useQuery<ILectureDetail>({
+  const handleOpenMetricModal = () => {
+    setMetricModalOpen(true);
+  };
+
+  const handleCloseMetricModal = () => {
+    setMetricModalOpen(false);
+  };
+
+  const {
+    data: lecture,
+    isLoading,
+    isError,
+  } = useQuery<ILectureDetail>({
     queryKey: ["lecture", id],
     queryFn: () => fetchLectureById(id as string),
     enabled: !!id,
@@ -44,31 +71,29 @@ function LectureDetails () {
 
   useEffect(() => {
     const fetchLectureView = async () => {
-      if (lecture && lecture.id) { 
+      if (lecture && lecture.id) {
         await viewLecture(lecture.id);
       }
     };
     fetchLectureView();
   }, [lecture]);
 
-
-  const { data: participants, refetch} = useQuery({
+  const { data: participants, refetch } = useQuery({
     queryKey: ["lectureParticipants", id],
     queryFn: () => fetchLectureParticipants(Number(id)),
     enabled: !!id,
   });
 
   const isParticipating = useMemo(() => {
-    return participants?.some(participant => participant.id === user?.id);
+    return participants?.some((participant) => participant.id === user?.id);
   }, [participants, user]);
-
 
   const deleteMutation = useMutation({
     mutationFn: () => deleteLecture(id as string),
     onSuccess: () => {
-      queryClient.invalidateQueries({queryKey: ["lectures", id]});
+      queryClient.invalidateQueries({ queryKey: ["lectures", id] });
       navigate("/lectures");
-    }
+    },
   });
 
   const handleDelete = () => {
@@ -76,7 +101,6 @@ function LectureDetails () {
   };
 
   const handleConfirm = () => {
-    
     deleteMutation.mutate();
     setShowDialog(false);
   };
@@ -85,16 +109,19 @@ function LectureDetails () {
     setShowDialog(false);
   };
 
-
   const participateMutation = useMutation({
     mutationFn: () => participateInLecture(id as string),
     onSuccess: () => {
       refetch();
       queryClient.invalidateQueries({ queryKey: ["lecture", id] });
-      setSuccessMessage("Você receberá em seu email informações sobre a palestra!");
+      setSuccessMessage(
+        "Você receberá em seu email informações sobre a palestra!"
+      );
     },
     onError: (error) => {
-      setErrorMessage(`Não é possivel participar dessa palestra. ERRO: ${error}`);
+      setErrorMessage(
+        `Não é possivel participar dessa palestra. ERRO: ${error}`
+      );
     },
   });
 
@@ -128,57 +155,70 @@ function LectureDetails () {
 
   type LectureStatus = "Agendada" | "Finalizada" | "Cancelada" | "Em andamento";
 
-
   const getStatusBgColor = (status: LectureStatus) => {
     switch (status) {
     case "Agendada":
-      return "bg-blue-500"; 
+      return "bg-blue-500";
     case "Finalizada":
-      return "bg-green-500"; 
+      return "bg-green-500";
     case "Cancelada":
-      return "bg-red-500"; 
+      return "bg-red-500";
     case "Em andamento":
-      return "bg-yellow-500"; 
+      return "bg-yellow-500";
     default:
-      return "bg-gray-200"; 
+      return "bg-gray-200";
     }
   };
 
   if (isLoading) return <LoadingSpinner />;
 
-  if (isError) return <ErrorNotification error="Erro ao carregar detalhes de palestra" />;
+  if (isError)
+    return <ErrorNotification error="Erro ao carregar detalhes de palestra" />;
 
   if (!lecture) return <p>Detalhes da palestra não encontrados.</p>;
-
 
   return (
     <div className="flex flex-col min-h-screen bg-gray-100">
       <Navbar />
       <div className="flex-grow flex items-center justify-center p-6 gap-4">
         <div className="max-w-6xl w-full bg-white shadow-md rounded-lg p-6">
-
           <div className="flex flex-col md:grid md:grid-cols-2 md:gap-6">
-
             <div className="md:mb-0">
-              <img className="object-cover w-full h-full rounded-lg" src={lecture.imageUrl} onError={(e) => e.currentTarget.src = "/images/heroBanner.png"} alt="Imagem de capa da palestra" />
+              <img
+                className="object-cover w-full h-full rounded-lg"
+                src={lecture.imageUrl}
+                onError={(e) =>
+                  (e.currentTarget.src = "/images/heroBanner.png")
+                }
+                alt="Imagem de capa da palestra"
+              />
             </div>
 
             <div className="flex flex-col">
-              <h1 className="text-3xl font-bold mb-4 md:mb-2">{lecture?.title}</h1>
+              <h1 className="text-3xl font-bold mb-4 md:mb-2">
+                {lecture?.title}
+              </h1>
               {renderIfNotEmpty(lecture?.description, () => (
-                <p className="text-lg text-gray-700 mb-2"><strong>Descrição:</strong> {lecture?.description}</p>
+                <p className="text-lg text-gray-700 mb-2">
+                  <strong>Descrição:</strong> {lecture?.description}
+                </p>
               ))}
             </div>
           </div>
 
           <div className="mt-6">
             {renderIfNotEmpty(lecture?.lecturer, () => (
-              <p className="text-lg text-gray-700 mb-2"><strong>Palestrante:</strong> {lecture?.lecturer}</p>
+              <p className="text-lg text-gray-700 mb-2">
+                <strong>Palestrante:</strong> {lecture?.lecturer}
+              </p>
             ))}
             {renderIfNotEmpty(lecture?.startsAt, () => (
               <p className="text-lg text-gray-700 mb-2">
                 <strong>Começo: </strong>
-                <time title={dateFormatted(lecture?.startsAt)} dateTime={lecture?.startsAt || ""}>
+                <time
+                  title={dateFormatted(lecture?.startsAt)}
+                  dateTime={lecture?.startsAt || ""}
+                >
                   {dateFormatted(lecture?.startsAt)}
                 </time>
               </p>
@@ -186,32 +226,51 @@ function LectureDetails () {
             {renderIfNotEmpty(lecture?.endsAt, () => (
               <p className="text-lg text-gray-700 mb-2">
                 <strong>Término: </strong>
-                <time title={dateFormatted(lecture?.endsAt)} dateTime={lecture?.endsAt || ""}>
+                <time
+                  title={dateFormatted(lecture?.endsAt)}
+                  dateTime={lecture?.endsAt || ""}
+                >
                   {dateFormatted(lecture?.endsAt)}
                 </time>
               </p>
             ))}
             {renderIfNotEmpty(lecture?.type, () => (
-              <p className="text-lg text-gray-700 mb-2"><strong>Tipo da palestra:</strong> {lecture?.type}</p>
+              <p className="text-lg text-gray-700 mb-2">
+                <strong>Tipo da palestra:</strong> {lecture?.type}
+              </p>
             ))}
             {renderIfNotEmpty(lecture?.status, () => (
-              <p className="text-lg inline-block text-gray-700 mb-2 " >
-                <strong>Status:</strong> <span className={`rounded-2xl p-2 text-white font-bold ${getStatusBgColor(lecture?.status as LectureStatus)}`}>{lecture?.status}</span>
+              <p className="text-lg inline-block text-gray-700 mb-2 ">
+                <strong>Status:</strong>{" "}
+                <span
+                  className={`rounded-2xl p-2 text-white font-bold ${getStatusBgColor(
+                    lecture?.status as LectureStatus
+                  )}`}
+                >
+                  {lecture?.status}
+                </span>
               </p>
             ))}
 
             {lecture?.url && (
               <p className="text-lg text-gray-700 mb-2">
                 <strong>URL:</strong>{" "}
-                <Link to={lecture.url} className="underline-animation text-[#861efd]">{lecture.url}</Link>
+                <Link
+                  to={lecture.url}
+                  className="underline-animation text-[#861efd]"
+                >
+                  {lecture.url}
+                </Link>
               </p>
             )}
 
-            {lecture?.address &&(
+            {lecture?.address && (
               <p className="text-lg text-gray-700 mb-2">
-                <strong>Endereço: </strong> 
+                <strong>Endereço: </strong>
                 <Link
-                  to={`https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(lecture?.address)}`}
+                  to={`https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(
+                    lecture?.address
+                  )}`}
                   target="_blank"
                   rel="noopener noreferrer"
                   className="underline-animation text-[#861efd]"
@@ -222,20 +281,27 @@ function LectureDetails () {
             )}
 
             {renderIfNotEmpty(lecture?.maximumCapacity, () => (
-              <p className="text-lg text-gray-700 mb-2"><strong>Quantidade máxima de participantes: </strong> {lecture?.maximumCapacity}</p>
+              <p className="text-lg text-gray-700 mb-2">
+                <strong>Quantidade máxima de participantes: </strong>{" "}
+                {lecture?.maximumCapacity}
+              </p>
             ))}
 
             {renderIfNotEmpty(lecture?.tags?.length, () => (
               <ul className="list-disc list-inside mb-4">
                 <strong>Tags:</strong>
-                {lecture?.tags.map((tag, index) => ( 
-                  <li key={tag.id || index} className="text-lg text-gray-700">{tag.name}</li>
+                {lecture?.tags.map((tag, index) => (
+                  <li key={tag.id || index} className="text-lg text-gray-700">
+                    {tag.name}
+                  </li>
                 ))}
               </ul>
             ))}
 
             {renderIfNotEmpty(lecture?.organizer.email, () => (
-              <p className="text-lg text-gray-700"><strong>Organizador:</strong> {lecture?.organizer.username}</p>
+              <p className="text-lg text-gray-700">
+                <strong>Organizador:</strong> {lecture?.organizer.username}
+              </p>
             ))}
 
             <div className="flex justify-between">
@@ -248,67 +314,77 @@ function LectureDetails () {
                     >
                       <TrashIcon className="h-6 w-6" />
                     </button>
-                    {isShowDialog && (
-                      <ConfirmationDialog
-                        message="Tem certeza que deseja deletar esta palestra?"
-                        onConfirm={handleConfirm}
-                        onCancel={handleCancel}
-                      />
-                    )}
-                    
+
                     <button
                       onClick={handleOpenEditModal}
                       className="mt-2 bg-blue-500 text-white font-bold p-2  rounded hover:bg-blue-600 duration-300 hover:scale-105 ml-2"
                     >
                       <PencilSquareIcon className="h-6 2-6" />
                     </button>
+                    <button
+                      onClick={handleOpenMetricModal}
+                      className="mt-2 bg-green-500 text-white font-bold p-2  rounded hover:bg-green-600 duration-300 hover:scale-105 ml-2"
+                    >
+                      <ChartBarIcon className="h-6 2-6" />
+                    </button>
                   </>
                 )}
               </div>
 
               <div>
-                {lecture?.status === "Finalizada" || lecture?.status === "Cancelada" ? null : (
-                  isParticipating ? (
+                {lecture?.status === "Finalizada" ||
+                lecture?.status === "Cancelada" ? null : isParticipating ? (
                     <button
                       onClick={handleUnparticipate}
                       className="mt-2 ml-2 bg-red-500 text-white font-bold p-2 rounded hover:bg-red-600 duration-300 hover:scale-105"
                     >
-          Sair da palestra
+                    Sair da palestra
                     </button>
                   ) : (
                     <button
                       onClick={handleParticipate}
                       className="mt-2 ml-2 bg-green-500 text-white font-bold p-2 rounded hover:bg-green-600 duration-300 hover:scale-105"
                     >
-          Participar
+                    Participar
                     </button>
-                  )
-                )}
+                  )}
               </div>
             </div>
 
+            {isShowDialog && (
+              <ConfirmationDialog
+                message="Tem certeza que deseja deletar esta palestra?"
+                onConfirm={handleConfirm}
+                onCancel={handleCancel}
+              />
+            )}
+
             {isEditModalOpen && (
               <Modal onClose={handleCloseEditModal}>
-                <LectureFormUpdate lecture={lecture} onClose={handleCloseEditModal} />
+                <LectureFormUpdate
+                  lecture={lecture}
+                  onClose={handleCloseEditModal}
+                />
+              </Modal>
+            )}
+
+            {isMetricModalOpen && (
+              <Modal onClose={handleCloseMetricModal}>
+                {user?.email === lecture?.organizer.email && (
+                  <>
+                    <MectricsSection user={user} lecture={lecture} />
+                  </>
+                )}
               </Modal>
             )}
           </div>
-
         </div>
-        
       </div>
-      <div className="flex justify-center mb-4">
-        {user?.email === lecture?.organizer.email && (
-          <>
-            <MectricsSection user={user} lecture={lecture} />
-          </>
-        )}
-      </div>
+      <div className="flex justify-center mb-4"></div>
       <Footer />
       <ErrorNotification error={errorMessage} />
       <SuccessNotification message={successMessage} />
     </div>
-
   );
 }
 
